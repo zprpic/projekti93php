ab6273f45b3fb7cbb27b814ece8f94cd0bda4251
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-07-27 12:40:07 +0200

    dates

--- 09_dates.php
+++ 09_dates.php
@@ -1,13 +1,27 @@
 <?php
 
 // Print current date
+echo date('Y-m-d H:i:s') . '<br>';
 
 // Print yesterday
+echo date('Y-m-d H:i:s', time() - 60 * 60 * 24) . '<br>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date('F j y, H:i:s') . '<br>';
 
 // Print current timestamp
+echo time().'<br>';
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
+$parsedDate = date_parse('2020-10-12 09:00:00');
+echo '<pre>';
+echo var_dump($parsedDate);
+echo '</pre>';
 
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 4 2020 12:45:35';
+
+$parsedDate = date_parse_from_format('F j Y H:i:s',$dateString);
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
