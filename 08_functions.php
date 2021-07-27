b22125430ebd2f5d6d62722a13b8d17a47bbcb95
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-07-27 12:32:32 +0200

    functions

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,39 @@
 <?php
 
 // Function which prints "Hello I am Zura"
-
+/* function hello() {
+    echo 'hello, im prle' . '<br>';
+}
+hello(); */
 // Function with argument
+/* function hello($name) {
+    return "hello, i am $name" . '<br>';
+}
+echo hello("bela");
+echo hello("prle");
+echo hello("mladen"); */
 
 // Create sum of two functions
+/* function sum($a, $b) {
+    return $a + $b;
+}
+
+echo sum(4, 5); */
 
 // Create function to sum all numbers using ...$nums
+/* function sum(...$nums) {
+    $sum = 0;
+    foreach($nums as $num) {
+        $sum += $num;
+    }
+    return $sum;
+}
+
+echo sum(1,2,3,4,5,6); */
 
 // Arrow functions
+function sum(...$nums) {
+return array_reduce($nums, fn($carry, $num) => $carry + $num);
+}
+
+echo sum(1,2,3,4,5,6);
